34464ceadff85ea6033c9a1faf7de6290950cfff
Nishithagoudp <144953399+Nishithagoudp@users.noreply.github.com>
2024-12-04 19:42:41 -0600

    Add files via upload

---
+++ database/migrations/2024_11_08_104546_add_code_snippet_to_responses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('responses', function (Blueprint $table) {
+            $table->text('code_snippet')->nullable()->after('body'); // Optional code snippet
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('responses', function (Blueprint $table) {
+            $table->dropColumn('code_snippet');
+
+        });
+    }
+};
